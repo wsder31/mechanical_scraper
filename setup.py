9541a76b1f55211fa8bdeb489bfbc637a1c985d5
silsako <silsako@naver.com>
2023-08-24 16:28:39 +0900

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'mechanical_scraper',
-    version = '2.0',
+    version = '3.2',
     description = 'Reduce your development time using this library based on requests and bs4.BeautifulSoup.',
     url = 'https://github.com/wsder31/mechanical_scraper.git',
     author = 'Silsako',
